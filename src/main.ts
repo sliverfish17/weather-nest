0bc9c436bcfea3050f073ded705ddd6d717385e4
sliverfish17 <a1708kolot@gmail.com>
2024-11-14 21:05:35 +0200

    app update for vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { MongoExceptionFilter } from 'utils/mongo-exception';
-
 import { AppModule } from './app.module';
+import * as serverless from 'serverless-http';
+import * as express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new MongoExceptionFilter());
   app.enableCors();
 
-  await app.listen(process.env.PORT);
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(expressApp);
